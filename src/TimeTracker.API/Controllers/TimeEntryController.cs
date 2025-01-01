443586988654735af0b5814763f34711a4267d78
dledvinka <ledvinka.david@gmail.com>
2025-01-01 23:07:36 +0100

    init

---
+++ src/TimeTracker.API/Controllers/TimeEntryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TimeTracker.Shared.Entities;
+
+namespace TimeTracker.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class TimeEntryController : ControllerBase
+{
+    private static List<TimeEntry> _entries = new List<TimeEntry>()
+    {
+        new TimeEntry() { Id = 1, Project = "Time Tracker app", End = DateTime.Now.AddHours(1)}
+    };
+
+    [HttpGet]
+    public ActionResult<List<TimeEntry>> GetAllTimeEntries()
+    {
+        return Ok(_entries);
+    }
+}
