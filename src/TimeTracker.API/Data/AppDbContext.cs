393d8bb58a67f8497dd34ca207b2c496b54c727c
dledvinka <ledvinka.david@gmail.com>
2025-01-02 21:02:47 +0100

    AppDbContext

---
+++ src/TimeTracker.API/Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿namespace TimeTracker.API.Data;
+
+using Microsoft.EntityFrameworkCore;
+using TimeTracker.Shared.Entities;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<TimeEntry> TimeEntries { get; set; }
+}
