0aaf72b01f6fc4267d95747fb91aeb66fef2ac80
dledvinka <ledvinka.david@gmail.com>
2025-01-02 21:15:11 +0100

    add connection string

--- src/TimeTracker.API/Program.cs
+++ src/TimeTracker.API/Program.cs
@@ -1,4 +1,5 @@
 using Scalar.AspNetCore;
+using TimeTracker.API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<ITimeEntryRepository, TimeEntryRepository>();
 builder.Services.AddScoped<ITimeEntryService, TimeEntryService>();
 
