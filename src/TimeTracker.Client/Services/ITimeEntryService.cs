ed516e553a84e6eb521c38b847a9a2f46b56b327
dledvinka <ledvinka.david@gmail.com>
2025-01-05 22:32:02 +0100

    TimeEntryService

---
+++ src/TimeTracker.Client/Services/ITimeEntryService.cs
@@ -0,0 +1,12 @@
+﻿using TimeTracker.Shared.Models.TimeEntry;
+
+namespace TimeTracker.Client.Services;
+
+public interface ITimeEntryService
+{
+    event Action? StateChanged;
+
+    List<TimeEntryResponse> TimeEntries { get; }
+
+    Task GetTimeEntriesByProject(int projectId);
+}
--- src/TimeTracker.Client/Services/TimeEntryService.cs
+++ src/TimeTracker.Client/Services/TimeEntryService.cs
@@ -1,5 +1,37 @@
-﻿namespace TimeTracker.Client.Services;
+﻿using System.Net.Http.Json;
+using TimeTracker.Shared.Models.TimeEntry;
 
-public class TimeEntryService
+namespace TimeTracker.Client.Services;
+
+public class TimeEntryService : ITimeEntryService
 {
+    private readonly HttpClient _httpClient;
+    public event Action? StateChanged;
+
+    public TimeEntryService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    public List<TimeEntryResponse> TimeEntries { get; private set; } = new();
+
+    public async Task GetTimeEntriesByProject(int projectId)
+    {
+        List<TimeEntryResponse>? result = null;
+
+        if (projectId <= 0)
+        {
+            result = await _httpClient.GetFromJsonAsync<List<TimeEntryResponse>>("api/TimeEntry");
+        }
+        else
+        {
+            result = await _httpClient.GetFromJsonAsync<List<TimeEntryResponse>>($"api/TimeEntry/project/{projectId}");
+        }
+
+        if (result != null)
+        {
+            TimeEntries = result;
+            StateChanged?.Invoke();
+        }
+    }
 }
