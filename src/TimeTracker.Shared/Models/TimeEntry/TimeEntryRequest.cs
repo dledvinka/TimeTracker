78c91648b0618c6a583fe81d62a443cd4525b8ef
dledvinka <ledvinka.david@gmail.com>
2025-01-09 21:35:11 +0100

    time entry validaton

--- src/TimeTracker.Shared/Models/TimeEntry/TimeEntryRequest.cs
+++ src/TimeTracker.Shared/Models/TimeEntry/TimeEntryRequest.cs
@@ -1,7 +1,10 @@
-﻿namespace TimeTracker.Shared.Models.TimeEntry;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeTracker.Shared.Models.TimeEntry;
 
 public class TimeEntryRequest
 {
+    [Range(1, int.MaxValue, ErrorMessage = "Project has to be set.")]
     public int ProjectId { get; set; }
     public DateTime Start { get; set; }
     public DateTime? End { get; set; }
