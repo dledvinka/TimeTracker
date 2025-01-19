38fbd08660dce063a10978a686af9dd10ed90f9b
dledvinka <ledvinka.david@gmail.com>
2025-01-19 19:27:43 +0100

    register

---
+++ src/TimeTracker.Client/Services/IAuthService.cs
@@ -0,0 +1,8 @@
+﻿namespace TimeTracker.Client.Services;
+
+using TimeTracker.Shared.Models.Account;
+
+public interface IAuthService
+{
+    Task Register(AccountRegistrationRequest request);
+}
--- src/TimeTracker.Shared/Models/Account/AccountRegistrationRequest.cs
+++ src/TimeTracker.Shared/Models/Account/AccountRegistrationRequest.cs
@@ -1,9 +1,15 @@
 ﻿namespace TimeTracker.Shared.Models.Account;
 
+using System.ComponentModel.DataAnnotations;
+
 public class AccountRegistrationRequest
 {
-    public required string UserName { get; set; }
-    public required string Email { get; set; }
-    public required string Password { get; set; }
-    public required string ConfirmPassword { get; set; }
+    [Required]
+    public string UserName { get; set; } = string.Empty;
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    [Required, MinLength(6)]
+    public string Password { get; set; } = string.Empty;
+    [Required, Compare(nameof(Password), ErrorMessage = "The passwords do not match.")]
+    public string ConfirmPassword { get; set; } = string.Empty;
 }
