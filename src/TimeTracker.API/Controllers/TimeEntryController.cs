41f537ba152be9b5366d908a568f5ed6acc7cb64
dledvinka <ledvinka.david@gmail.com>
2025-01-19 17:55:37 +0100

    [Authorize] added

--- src/TimeTracker.API/Controllers/TimeEntryController.cs
+++ src/TimeTracker.API/Controllers/TimeEntryController.cs
@@ -1,9 +1,11 @@
 ﻿namespace TimeTracker.API.Controllers;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 [Route("api/[controller]")]
 [ApiController]
+[Authorize]
 public class TimeEntryController : ControllerBase
 {
     private readonly ITimeEntryService _timeEntryService;
