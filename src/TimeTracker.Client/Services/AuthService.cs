d998080b2b05674a429cc17c43ba94b5f1ef6f08
dledvinka <ledvinka.david@gmail.com>
2025-01-19 19:45:28 +0100

    toasts

--- src/TimeTracker.Client/Services/AuthService.cs
+++ src/TimeTracker.Client/Services/AuthService.cs
@@ -1,19 +1,43 @@
 ﻿namespace TimeTracker.Client.Services;
 
 using System.Net.Http.Json;
+using Blazored.Toast.Services;
 using TimeTracker.Shared.Models.Account;
 
 public class AuthService : IAuthService
 {
     private readonly HttpClient _httpClient;
+    private readonly IToastService _toastService;
 
-    public AuthService(HttpClient httpClient)
+    public AuthService(HttpClient httpClient, IToastService toastService)
     {
         _httpClient = httpClient;
+        _toastService = toastService;
     }
 
     public async Task Register(AccountRegistrationRequest request)
     {
-        await _httpClient.PostAsJsonAsync("api/account", request);
+        var result = await _httpClient.PostAsJsonAsync("api/account", request);
+
+        if (result is not null)
+        {
+            var response = await result.Content.ReadFromJsonAsync<AccountRegistrationResponse>();
+
+            if (!response.IsSuccess && response.Errors is not null)
+            {
+                foreach (var responseError in response.Errors)
+                {
+                    _toastService.ShowError(responseError);
+                }
+            }
+            else if (!response.IsSuccess)
+            {
+                _toastService.ShowError("An error occurred while processing your request");
+            }
+            else
+            {
+                _toastService.ShowSuccess("Account created successfully");
+            }
+        }
     }
 }
