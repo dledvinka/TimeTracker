3541ca3265bfb36486cd86c773316bef31225ea9
dledvinka <ledvinka.david@gmail.com>
2025-01-28 08:07:26 +0100

    Login page styling

--- src/TimeTracker.Shared/Models/Login/LoginRequest.cs
+++ src/TimeTracker.Shared/Models/Login/LoginRequest.cs
@@ -1,7 +1,12 @@
 ﻿namespace TimeTracker.Shared.Models.Login;
 
+using System.ComponentModel.DataAnnotations;
+
 public class LoginRequest
 {
+    [Required]
     public string UserName { get; set; } = string.Empty;
+
+    [Required]
     public string Password { get; set; } = string.Empty;
 }
